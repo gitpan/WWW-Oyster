0d5b9e593ca93479e0b4ebe2350d255343c5919f
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-05-03 09:19:16 +0000

    Import of MANWAR/WWW-Oyster-0.04 from CPAN.
    gitpan-cpan-distribution: WWW-Oyster
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MANWAR/WWW-Oyster-0.04.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/WWW/Oyster.pm
+++ lib/WWW/Oyster.pm
@@ -14,11 +14,11 @@ WWW::Oyster - Interface to Oyster Account.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 Readonly my $LOGIN  => 'https://oyster.tfl.gov.uk/oyster/entry.do';
 Readonly my $LOGOUT => 'https://oyster.tfl.gov.uk/oyster/oysterlogout.do';
